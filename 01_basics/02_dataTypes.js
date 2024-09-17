66ad06b1aef483242d397aab71b5e5cb50b74c3f
Komal21488 <komalkardam21647@gmail.com>
2024-09-17 09:03:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";//treat all JS code as a newer version(means all new syntax are used..)
+
+// alert( 3 + 3 ) //we are using nodejs,not browser
+
+console.log(3 
+    + 3)  //code readability should be high..
+
+console.log("KOMAL")
+
+let name = "KOMAL"       //string datatype
+let age = 21             //numbers value
+let isLoggedIn = false     //boolean type
+let state;
+
+/*PRIMITIVE DATATYPES
+//number =>2 to power 53 (range)number dataType
+//bigint (used in large numbers like stockmarketing,trading,facebook)
+//string => ""
+//boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol(find uniqueness) =>unique
+*/
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      // object
+
+
+
+
