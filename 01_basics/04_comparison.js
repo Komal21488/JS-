55345e20f5a9464d5c597f39c0b2395cb513561b
Komal21488 <komalkardam21647@gmail.com>
2024-09-19 16:12:01 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+/*console.log( 2 > 1);
+console.log( 2 >= 1);
+console.log( 2 < 1);
+console.log( 2 == 1);
+console.log( 2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);  */
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+*/
+// ===( means strict check)
+
+console.log("2" == 2);     // true
+console.log("2" === 2);     // false (check dataType  ("2"string) (2 number))
+
+
+
