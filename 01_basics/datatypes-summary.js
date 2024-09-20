311a8c105db3aa497c3b9a58a80f6b46f4685e4b
Komal21488 <komalkardam21647@gmail.com>
2024-09-20 12:25:14 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+// Reference (Non-Primtive)
+ 
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "KOMAL",
+    age: 21,
+}
+
+//     function(){}     (function definition)
+const myFunction = function(){
+    console.log("Hello World!");
+    
+}
+
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof outsideTemp);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
+
+
