015f33df78515c708e4d20e58e15d4a850fb5d96
Komal21488 <komalkardam21647@gmail.com>
2024-09-23 08:11:39 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -39,6 +39,30 @@ console.log(typeof bigNumber);
 console.log(typeof heros);
 console.log(typeof myObj);
 console.log(typeof myFunction);
+console.log(typeof userEmail);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename 
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "komal@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
