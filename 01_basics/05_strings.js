49f87bf39539ee6c2a4731266f30c099254c47b8
Komal21488 <komalkardam21647@gmail.com>
2024-09-25 11:14:56 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name  = "Komal"
+const repoCount = 10
+
+// console.log(name + repoCount + "  Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Komalkk')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('K'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-4, 4)
+console.log(anotherString);
+
+
+
